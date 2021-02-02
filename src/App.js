cce81ba0111e34764a2ef892d15beabd6342b837
pasqualeparillo <patparillo@gmail.com>
2021-02-02 09:21:41 -0800

    react_tailwind_boilerplate

---
+++ src/App.js
@@ -0,0 +1,11 @@
+// Dependencies
+import React from "react";
+import "./style.css";
+// Styles
+import "./tailwind.output.css";
+
+const App = () => {
+  return <div className="min-h-screen relative flex flex-col relative"></div>;
+};
+
+export default App;
